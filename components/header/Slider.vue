22d10dc5b60c0d6f1781f153eb6615e4c83ccf2f
thaycacac <thaycacac@gmail.com>
2018-08-22 01:32:13 +0700

    upload slider

--- components/header/Slider.vue
+++ components/header/Slider.vue
@@ -1,40 +1,25 @@
 <template>
     <div>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
-        <p>.</p>
+        <header class="masthead text-center text-white d-flex">
+            <div class="container my-auto">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto">
+                        <h1 class="text-uppercase">
+                            <strong>Your Favorite Source of Free Bootstrap Themes</strong>
+                        </h1>
+                        <hr>
+                    </div>
+                    <div class="col-lg-8 mx-auto">
+                        <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework ! Just download
+                            your template and start going, no strings attached !</p>
+                        <a class="btn btn-primary btn-xl js-scroll-trigger"
+                            href="#about">Find Out More</a>
+                    </div>
+                </div>
+            </div>
+        </header>
     </div>
 </template>
 <style lang="scss" scoped>
+    @import '../../assets/sass/main.sass';
 </style>
