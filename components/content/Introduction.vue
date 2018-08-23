f29606f05eb412686532f118cb332ca2bca1d85a
thaycacac <thaycacac@gmail.com>
2018-08-24 01:29:16 +0700

    upload style troduction

--- components/content/Introduction.vue
+++ components/content/Introduction.vue
@@ -1,22 +1,42 @@
 <template>
     <div class="container-intro">
-        <h1 class="title">A DIGITAL STUDIO CRAFTING BEAUTIFUL EXPERIENCES.</h1>
-        <h5 class="title  is-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h5>
+        <h1 class="title">WELCOME TO DEFINITY</h1>
+        <h5 class="title  is-5">CREATIVE MULTIPURPOSE ONE/MULTI PAGE TEMPLATE</h5>
         <p>
-            We've been crafting beautiful websites, launching stunning brands and making clients happy for years. With our prestigious
-            craftsmanship, remarkable client care and passion for design.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
         </p>
     </div>
 </template>
 <style lang="scss" scoped>
 @import '../../assets/sass/main.sass';
-.container-intro {
+.container-intro{
   width: 60%;
   margin: 150px auto;
   text-align: center;
-  font-family: $font-opensan;
+  font-family: $font-montserrat;
   h1 {
-    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    letter-spacing: 4px;
+  }
+  h5 {
+    color: #999;
+    font-family: $font-opensan;
+    font-weight: 300;
+    letter-spacing: 2.5px;
+    text-transform: uppercase;
+    font-size: 14px;
+  }
+  p {
+    color: #777;
+    font-family: $font-opensan;
+    font-size: 14px;
+    font-weight: 300;
+    letter-spacing: 0.2px;
+    line-height: 1.8em;
+    margin-top: 90px;
   }
 }
 </style>
