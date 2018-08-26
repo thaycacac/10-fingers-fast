688ef48a88ce84aac4a3aa1dd2a6d0f166735649
thaycacac <thaycacac@gmail.com>
2018-08-26 22:02:28 +0700

    done back ground fix

--- components/content/Background.vue
+++ components/content/Background.vue
@@ -1,10 +1,70 @@
 <template>
-    <div class="cd-fixed-bg cd-fixed-bg-image">
-        <h2>Lorem ipsum dolor sit amet.</h2>
+    <div class="is-fixed-bg is-fixed-bg-image">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <one-view
+            :icon="icon.icon1"
+            :content="content.content1"
+            :description="description.description1"></one-view>
+          </div>
+          <div class="column">
+            <one-view
+            :icon="icon.icon2"
+            :content="content.content2"
+            :description="description.description2"></one-view>
+          </div>
+          <div class="column">
+            <one-view
+            :icon="icon.icon3"
+            :content="content.content3"
+            :description="description.description3"></one-view>
+          </div>
+          <div class="column">
+            <one-view
+            :icon="icon.icon4"
+            :content="content.content4"
+            :description="description.description4"></one-view>
+          </div>
+        </div>
+        </div>
     </div>
 </template>
+
+<script>
+  import OneView from './background/view.vue';
+  export default {
+    data () {
+      return {
+        icon: {
+          icon1: 'fas fa-heart',
+          icon2: 'fas fa-smile',
+          icon3: 'fas fa-anchor',
+          icon4: 'fas fa-comments'
+        },
+        content: {
+          content1: 516,
+          content2: 478,
+          content3: 652,
+          content4: 458
+        },
+        description: {
+          description1: 'pizzas Ordered',
+          description2: 'happy clients',
+          description3: 'projects completed',
+          description4: 'comments reserved'
+        }
+      }
+    },
+    components: {
+      OneView
+    }
+  };
+  
+</script>
+
 <style lang="scss" scoped>
-.cd-fixed-bg {
+.is-fixed-bg {
   position: relative;
   height: 300px;
   background-size: cover;
@@ -28,7 +88,7 @@
     font-size: 3rem;
   }
 }
-.cd-fixed-bg.cd-fixed-bg-image {
+.is-fixed-bg.is-fixed-bg-image {
   background-image: url(https://i.imgur.com/ZG50ZEA.jpg);
 }
 </style>
---
+++ components/content/background/view.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="is-one-component">
+       <i :class="icon"></i>
+       <p class="is-number">{{ content }}</p>
+       <p class="is-description">{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "../../../assets/sass/main.sass";
+.is-one-component {
+  color: white;
+  font-size: 45px;
+  text-align: center;
+  font-weight: 50;
+  i {
+    margin-top: 35px;
+    color: white;
+  }
+  .is-number {
+    font-size: 55px;
+    font-weight: 600;
+    font-family: $font-opensan;
+  }
+  .is-description {
+    font-size: 12px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    color: #ababab;
+  }
+}
+</style>
