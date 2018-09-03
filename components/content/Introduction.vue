7d4572f27c8c040f4f3040e6849b7661c407284e
thaycacac <thaycacac@gmail.com>
2018-09-04 02:05:16 +0700

    add image slider

--- components/content/Introduction.vue
+++ components/content/Introduction.vue
@@ -3,10 +3,10 @@
         <h1 class="title">WELCOME TO DEFINITY</h1>
         <h5 class="title  is-5">CREATIVE MULTIPURPOSE ONE/MULTI PAGE TEMPLATE</h5>
         <p>
-            Cộng hòa xã hội chủ nghĩa việt nam
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            Bạn mới sử dụng máy tính và đang muốn tìm hiểu xem làm thế nào để 
+            có thể gõ phím bằng 10 ngón nhanh? Vậy thì không thể bỏ qua bài viết và các bài tập của chúng 
+            tôi với các kỹ năng gõ phím 10 ngón nhanh người mới nên biết. Hy vọng sẽ là giải pháp hữu 
+            ích giúp bạn cải thiện cũng như luyên tập thêm về kỹ năng đánh máy này!
         </p>
     </div>
 </template>
