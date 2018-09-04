0fe2a279917f1e53a8e6f087d3cd4a77c1927f62
thaycacac <thaycacac@gmail.com>
2018-09-04 13:25:08 +0700

    done content what do you can

--- components/content/Target.vue
+++ components/content/Target.vue
@@ -2,24 +2,27 @@
     <div class="my-target">
         <div class="container-intro">
             <h1 class="title">Bạn có thể làm gì?</h1>
-            <h5 class="title  is-5 my-descrip">CREATIVE MULTIPURPOSE ONE/MULTI PAGE TEMPLATE</h5>
+            <h5 class="title  is-5 my-descrip">Đến với 10FingersFast, chúng tôi có thể giúp bạn</h5>
         </div>
         <div class="container">
             <div class="columns">
                 <div class="column">
                     <one-view
-                    :title = title1
-                    :content = content1></one-view>
+                    :icon=icon.icon1
+                    :title = title.title1
+                    :content = content.content1></one-view>
                 </div>
                 <div class="column">
                     <one-view
-                    :title = title2
-                    :content = content2></one-view>
+                    :icon=icon.icon2
+                    :title = title.title2
+                    :content = content.content2></one-view>
                 </div>
                 <div class="column">
                     <one-view
-                    :title = title3
-                    :content = content3></one-view>
+                    :icon=icon.icon3
+                    :title = title.title3
+                    :content = content.content3></one-view>
                 </div>
             </div>
         </div>
@@ -30,12 +33,21 @@
   export default {
     data () {
       return {
-        title1 : 'Hướng dẫn',
-        content1: 'Lorem ipsum dolor sit amet, consectetur elit adipisicing , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimv',
-        title2: 'Multiple icon sets',
-        content2: 'Lorem ipsum dolor sit amet, consectetur elit adipisicing , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimv',
-        title3: 'Lots of features',
-        content3: 'Lorem ipsum dolor sit amet, consectetur elit adipisicing , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimv'
+        title: {
+          title1 : 'Học',
+          title2: 'Hành',
+          title3: 'Chơi',
+        },
+        content: {
+          content1: 'Tìm hiểu các khái niệm cơ bản của phương pháp 10 ngón tay chỉ trong vài phút - để có cái nhìn thoáng qua đầu tiên hoặc để bắt đầu thực hành đánh máy ngay lập tức.',
+          content2: 'Thực hành các bài luyện gõ chữ 10 ngón từ cơ bản đến nâng cao giúp bạn có thể hình thành thói quen gõ 10 ngón một cách chính xác và điêu luyện. ',
+          content3: 'Các trò chơi và các bài test giúp bạn hứng thú hơn trong quá trình học và kiểm tra trình độ của bạn, sự tiến bộ của bạn đến đâu...'
+        },
+        icon: {
+           icon1: 'fas fa-bookmark',
+           icon2: 'fas fa-graduation-cap',
+           icon3: 'fas fa-layer-group'
+        }
       }
     },
     components: {
@@ -50,7 +62,7 @@
   @include setupTitle();
 }
 .my-target {
-  padding-bottom: 80px;
+  padding-bottom: 120px;
   background-color: #f8f8f8;
 }
 </style>
--- components/content/target/view.vue
+++ components/content/target/view.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="my-target">
-       <img src="../../../assets/img/target.png" alt="target">
-       <h5 class="title">{{ title }}</h5>
-       <p>{{ content }}</p>
+    <div class="is-target">
+      <i :class="icon"></i>
+      <h5 class="title">{{ title }}</h5>
+      <p>{{ content }}</p>
     </div>
 </template>
 
@@ -16,6 +16,10 @@
       content: {
         type: String,
         required: true
+      },
+      icon: {
+        type: String,
+        required: true
       }
     }
   }
@@ -23,38 +27,6 @@
 
 <style lang="scss" scoped>
 @import '../../../assets/sass/main.sass';
-.my-target {
-  background-color: white;
-  padding: 50px 40px;
-  border: 1px solid #f8f8f8;
-  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.2);
-  transition: all 0.3s;
-  -webkit-transition: all 0.3s;
-  border-radius: 15px;
-  &:hover {
-    animation-duration: 0.5s;
-    margin-top: -5px;
-    background-size: 400% 400%;
-    color: white !important;
-    background: linear-gradient($randomNumber + deg, rgb(226, 226, 226), #ffffff);
-    cursor: pointer;
-  }
-  h5{
-    margin-top: 40px;
-    margin-bottom: 30px;
-    font-size: 1.07em;
-    letter-spacing: 3px;
-    font-family: $font-montserrat;
-    color: #111;
-    text-transform: uppercase;}
-  p{
-    font-family: $font-opensan;
-    font-size: 14px;
-    font-weight: 300;
-    letter-spacing: 0.2px;
-    line-height: 1.8em;
-    color: $font-color-price;
-  }
-}
+
 </style>
 
