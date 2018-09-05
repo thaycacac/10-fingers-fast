c01c64ff08c9f668e42b5771efd45f92221ada58
thaycacac <thaycacac@gmail.com>
2018-09-05 12:47:43 +0700

    add login page

---
+++ pages/login.vue
@@ -0,0 +1,57 @@
+<template>
+    <section class="hero is-success is-fullheight">
+        <navbar></navbar>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Đăng Nhập</h3>
+                    <div class="box">
+                        <figure class="avatar">
+                            <img src="https://placehold.it/128x128">
+                        </figure>
+                        <form>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="email" placeholder="Nhập email" autofocus="">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="password" placeholder="Nhập mật khẩu">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <label class="checkbox">
+                                    <input type="checkbox">
+                                    Nhớ mật khẩu
+                                </label>
+                            </div>
+                            <button class="button is-block is-info is-large is-fullwidth">Login</button>
+                        </form>
+                    </div>
+                    <p class="has-text-grey">
+                        <a href="../">Đăng ký</a> &nbsp;·&nbsp;
+                        <a href="../">Quên mật khẩu</a> &nbsp;·&nbsp;
+                        <a href="../">Giúp?</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style lang="scss" scoped>
+@import '../assets/sass/user/login.sass';
+@import '../assets/sass/main.sass';
+nav.navbar.is-transparent.is-fixed-top {
+  background: $color-primary;
+}
+</style>
+<script>
+import Navbar from '../components/header/Navbar'
+export default {
+  components: {
+    Navbar
+  } 
+}
+</script>
+
