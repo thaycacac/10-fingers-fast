2c8141c6e3fef51290cab500fcb2d9c7b56a7d9a
thaycacac <thaycacac@gmail.com>
2018-09-10 16:08:15 +0700

    add front-end show lesson

---
+++ components/content/Learn.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="learn">
+    <div class="time-line">
+      <div class="__container">
+        <div class="__block">
+          <div class="__img --picture">
+            <img src="/sgv/timeline-picture.svg" alt="Picture">
+          </div>
+          <div class="__content">
+            <h2>Title of section 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi cupiditate ut corporis accusamus laudantium eveniet aliquid fuga, maxime, quibusdam distinctio voluptatum consectetur? Recusandae commodi delectus autem ipsa? Dolores, provident ex!</p>
+            <a href="#" class="__read-more">Read more</a>
+            <span class="__date">Jan14</span>
+          </div>
+        </div>
+
+        <div class="__block">
+          <div class="__img --movie">
+            <img src="/sgv/timeline-picture.svg" alt="Picture">
+          </div>
+
+          <div class="__content">
+            <h2>Title of section 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi cupiditate ut corporis accusamus laudantium eveniet aliquid fuga, maxime, quibusdam distinctio voluptatum consectetur? Recusandae commodi delectus autem ipsa? Dolores, provident ex!</p>
+            <a href="#" class="__read-more">Read more</a>
+            <span class="__date">Jan14</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+@import '../../assets/sass/main.sass';
+@import '../../assets/sass/learn/_lession.scss';
+</style>
+
+
---
+++ pages/learn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <navbar/>
+    <learn/>
+  </div>
+</template>
+<script>
+import Learn from '../components/content/Learn'
+import Navbar from '../components/header/Navbar'
+export default {
+  components: {
+    Navbar,
+    Learn,
+  }
+}
+
+</script>
