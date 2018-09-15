55b6df6a1c0f5609d5686205c09c96f64b2615fb
thaycacac <thaycacac@gmail.com>
2018-09-15 22:21:30 +0700

    [update] animation error in login

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,55 +1,57 @@
 <template>
-    <section class="hero is-success is-fullheight is-login">
-        <navbar></navbar>
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-black">Đăng Nhập</h3>
-                    <div class="box">
-                        <figure class="avatar">
-                            <img src="../assets/img/user/logo-login.png">
-                        </figure>
-                        <p style="color: red" v-show="checkError">{{ error }}</p>
-                        <form>
-                            <div class="field">
-                                <div class="control">
-                                    <input 
-                                    class="input is-large" 
-                                    type="text" 
-                                    placeholder="Nhập tên tài khoản" 
-                                    autofocus="" 
-                                    v-model="username">
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <input 
-                                    class="input is-large" 
-                                    type="password" 
-                                    placeholder="Nhập mật khẩu" 
-                                    v-model="password">
-                                </div>
-                            </div>
-                            <div class="field">
-                                <label class="checkbox">
-                                    <input type="checkbox">
-                                    Nhớ mật khẩu
-                                </label>
-                            </div>
-                            <button 
-                            class="button is-block is-warning is-large is-fullwidth" 
-                            @click.prevent="signin">Đăng Nhập</button>
-                        </form>
-                    </div>
-                    <p class="has-choose-more">
-                        <a href="../">Đăng ký</a> &nbsp;·&nbsp;
-                        <a href="../">Quên mật khẩu</a> &nbsp;·&nbsp;
-                        <a href="../">Giúp?</a>
-                    </p>
+  <section class="hero is-success is-fullheight is-login">
+    <navbar></navbar>
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <h3 class="title has-text-black">Đăng Nhập</h3>
+          <div class="box">
+            <figure class="avatar">
+              <img src="../assets/img/user/logo-login.png">
+            </figure>
+            <transition name="bounce">
+              <p style="color: red" v-show="checkError">{{ error }}</p>
+            </transition>
+            <form>
+              <div class="field">
+                <div class="control">
+                  <input 
+                  class="input is-large" 
+                  type="text" 
+                  placeholder="Nhập tên tài khoản" 
+                  autofocus="" 
+                  v-model="username">
                 </div>
-            </div>
+              </div>
+              <div class="field">
+                <div class="control">
+                  <input 
+                  class="input is-large" 
+                  type="password" 
+                  placeholder="Nhập mật khẩu" 
+                  v-model="password">
+                </div>
+              </div>
+              <div class="field">
+                <label class="checkbox">
+                  <input type="checkbox">
+                  Nhớ mật khẩu
+                </label>
+              </div>
+              <button 
+              class="button is-block is-warning is-large is-fullwidth" 
+              @click.prevent="signin">Đăng Nhập</button>
+            </form>
+          </div>
+          <p class="has-choose-more">
+            <a href="../">Đăng ký</a> &nbsp;·&nbsp;
+            <a href="../">Quên mật khẩu</a> &nbsp;·&nbsp;
+            <a href="../">Giúp?</a>
+          </p>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -101,6 +103,7 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/sass/user/signin.sass';
 @import '../assets/sass/main.sass';
-@import '../assets/sass/main.sass';
+@import '../assets/sass/animation.scss';
 @include setupBackgroundNav();
+@include setupAnimationError();
 </style>
