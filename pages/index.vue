7dac56d92283e5a1781a88a8940fd007ce5a90e9
Thaycacac <thaycacac@gmail.com>
2018-09-21 21:35:36 +0700

    [add] layout homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <navbar></navbar>
-    <slider></slider>
     <introduction></introduction>
     <navigation></navigation>
     <target></target>
@@ -9,14 +7,10 @@
     <background></background>
     <typing-study></typing-study>
     <follow></follow>
-    <my-footer></my-footer>
   </div>
 </template>
 
 <script>
-// import axios from '~/plugins/axios'
-import Navbar from "../components/header/Navbar"
-import Slider from "../components/header/Slider"
 import Introduction from '../components/content/Introduction'
 import Navigation from '../components/content/Navigation'
 import Target from '../components/content/Target'
@@ -24,12 +18,10 @@ import Background from '../components/content/Background'
 import Guide from '../components/content/Guide'
 import Follow from '../components/content/Follow'
 import TypingStudy from '../components/content/TypingStudy'
-import MyFooter from '../components/footer/Footer';
 
 export default {
+  layout: 'homepage',
   components: {
-    Navbar,
-    Slider,
     Introduction,
     Navigation,
     Target,
@@ -37,7 +29,6 @@ export default {
     Guide,
     Follow,
     TypingStudy,
-    MyFooter,
   }
 }
 </script>
---
+++ pages/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <introduction/>
+</template>
+<script>
+import Introduction from '../components/content/Introduction'
+export default {
+  components: {
+    Introduction
+  }
+}
+
+</script>
