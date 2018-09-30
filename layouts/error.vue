96aa8a2856781f41eb393fc7d50d00e803391e8c
Thaycacac <thaycacac@gmail.com>
2018-10-01 03:59:53 +0700

    [Update] page error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,37 +1,222 @@
 <template>
-  <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
-    </nuxt-link>
+  <section class="page-error">
+    <h1 class="title is-2">10 FingersFast</h1>
+    <p id="error">{{ error.statusCode }}</p>
+    <p id="explanation">{{ error.message }}.</p>
+    <div id="container">
+      <div class="server" id="server1">
+        <div class="hdd-container">
+          <div class="hdd">
+            <div class="led green-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led green-led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+        </div>
+        <div class="cooling-system">
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+        </div>
+      </div>
+      <div class="server" id="server2">
+        <div class="hdd-container">
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led "></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+        </div>
+        <div class="cooling-system">
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+        </div>
+      </div>
+      <div class="server" id="server3">
+        <div class="hdd-container">
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+          <div class="hdd">
+            <div class="led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led red-led"></div>
+            <div class="led"></div>
+          </div>
+        </div>
+        <div class="cooling-system">
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+          <div class="grid"></div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 <script>
-export default {
-  props: ['error']
-}
+  export default {
+    props: ['error']
+  }
 </script>
 
 <style scoped>
-.title
-{
-  margin-top: 15px;
-  font-size: 5em;
-}
-.info
-{
-  font-weight: 300;
-  color: #9aabb1;
-  margin: 0;
-}
-.button
-{
-  margin-top: 50px;
-}
+@import '../assets/sass/error/error.scss';
 </style>
