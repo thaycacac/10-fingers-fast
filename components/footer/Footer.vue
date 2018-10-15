33f27710d2d90fb974745c8141c6767e779d5289
Thaycacac <thaycacac@gmail.com>
2018-10-16 00:14:16 +0700

    [Update] Footer with pug

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -1,22 +1,32 @@
-<template>
-  <footer class="footer is-footer">
-    <div class="content has-text-centered">
-      <p>
-        Copyright © 2018 <strong><a href="https://github.com/thaycacac" class="__link-footer">THAYCACAC</a></strong>.
-      </p>
-      <div align="center" class="has-text-centered">
-      <ul>
-        <li><a href="https://facebook.com/thaycacac"><i class="fab fa-facebook"></i></a></li>
-        <li><a href="https://twitter.com/Thaycacac"><i class="fab fa-twitter"></i></a></li>
-        <li><a href="https://www.instagram.com/thaycacac/"><i class="fab fa-instagram"></i></a></li>
-        <li><a href="mailto:thaycacac@gmail.com?Subject=Hello%20Thaycacac" target="_top"><i class="fas fa-envelope"></i></a></li>
-        <li><a href="https://github.com/thaycacac"><i class="fab fa-github-square"></i></a></li>
-        <li><a href="https://www.youtube.com/channel/UCSfVU0_rcB5Kn1SkYrcTbRw"><i class="fab fa-youtube-square"></i></a></li>
-        <li><a href="https://www.linkedin.com/in/Thaycacac"><i class="fab fa-linkedin"></i></a></li>
-      </ul>
-      </div>
-    </div>
-  </footer>
+<template lang="pug">
+  footer.footer.is-footer
+    div.content.has-text-centered
+      p Copyright © 2018 
+        strong
+          a.__link-footer(href="https://github.com/thaycacac") THAYCACAC
+      div.has-text-centered(align="center")
+      ul
+        li
+          a(href="https://facebook.com/thaycacac")
+            i.fab.fa-facebook
+        li
+          a(href="https://twitter.com/Thaycacac")
+            i.fab.fa-twitter
+        li
+          a(href="https://www.instagram.com/thaycacac")
+            i.fab.fa-instagram
+        li
+          a(href="mailto:thaycacac@gmail.com?Subject=Hello%20Thaycacac" target="_top")
+            i.fas.fa-envelope
+        li
+          a(href="https://github.com/thaycacac")
+            i.fab.fa-github-square
+        li
+          a(href="https://www.youtube.com/channel/UCSfVU0_rcB5Kn1SkYrcTbRw")
+            i.fab.fa-youtube-square
+        li
+          a(href="https://www.linkedin.com/in/Thaycacac")
+            i.fab.fa-linkedin
 </template>
 <style lang="sass" scoped>
 @import '~/assets/sass/main.sass'
